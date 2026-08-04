f7d5890f8cd8baddfe2a4aa4b9caf0210a5c104e
voxelmagpie <voxelmagpie@mailbox.org>
2026-08-04 14:19:55 +0100

    chess_demo.sh
    Script no longer runs the generated program as it is intended to be run
    by a chess GUI program, not directly by the user.

--- chess/chess_demo.sh
+++ chess/chess_demo.sh
@@ -1,4 +1,4 @@
 # Run this from the parent dir
 set -e
-time -f "Took %es" cabal run --disable-optimization --enable-debug-info --enable-profiling --ghc-options="-fprof-auto-calls" moorhen -- build chess/chess.mh --builtins-path ./builtins.mh --stlib-path ./stlib --out-dir ./out --timings +RTS -N
-node out/chess.mjs
+time -f "Compiling chess engine demo took %es" cabal run --disable-optimization --enable-debug-info --enable-profiling --ghc-options="-fprof-auto-calls" moorhen -- build chess/chess.mh --builtins-path ./builtins.mh --stlib-path ./stlib --out-dir ./out --timings +RTS -N
+
